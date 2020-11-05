6af16cd8976ab9ec6daf35f21b9e024810c9b7db
hemang11 <hemangshrimali22@gmail.com>
2020-11-05 17:43:33 +0530

    initial commit

---
+++ quill/quill.js
@@ -0,0 +1,69 @@
+/* eslint-env browser */
+
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { QuillBinding } from 'y-quill'
+import Quill from 'quill'
+import QuillCursors from 'quill-cursors'
+
+Quill.register('modules/cursors', QuillCursors)
+
+window.addEventListener('load', () => {
+  const ydoc = new Y.Doc()
+  const provider = new WebsocketProvider('wss://demos.yjs.dev', 'quill', ydoc)
+  const type = ydoc.getText('quill')
+  const editorContainer = document.createElement('div')
+  editorContainer.setAttribute('id', 'editor')
+  document.body.insertBefore(editorContainer, null)
+
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['clean']  
+  ];
+  var editor = new Quill(editorContainer, {
+    modules: {
+      cursors : true,
+      toolbar:toolbarOptions,
+      history: {
+        userOnly: true
+      }
+    },
+    placeholder: 'Start collaborating...',
+    theme: 'snow' // or 'bubble'
+  })   
+
+  const binding = new QuillBinding(type, editor, provider.awareness)
+
+  provider.awareness.setLocalStateField('user', {
+    name: 'Typing Jimmy',
+    color: 'blue'
+  });
+
+  const connectBtn = document.getElementById('y-connect-btn')
+  connectBtn.addEventListener('click', () => {
+    if (provider.shouldConnect) {
+      provider.disconnect()
+      connectBtn.textContent = 'Connect'
+    } else {
+      provider.connect()
+      connectBtn.textContent = 'Disconnect'
+    }
+  })
+
+  // @ts-ignore
+  window.example = { provider, ydoc, type, binding, Y }
+})
